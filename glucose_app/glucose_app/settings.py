938805efbe685781af265059c5825742717a282c
luciano-s <lcsjr70@gmail.com>
2023-03-11 17:20:04 -0300

    refactor(env): put django secret on .env

--- glucose_app/glucose_app/settings.py
+++ glucose_app/glucose_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w)f!n%@pyae4g9cxty-d!f7-3)^9w1qrg)(^af1ju1f=q%h&k2"
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
