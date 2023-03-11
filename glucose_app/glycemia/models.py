85b19ae98a78d5439b39d0a4e8e805ec7b1f4512
luciano-s <lcsjr70@gmail.com>
2023-03-11 17:33:20 -0300

    fix(glycemia): add related name

--- glucose_app/glycemia/models.py
+++ glucose_app/glycemia/models.py
@@ -3,7 +3,9 @@ from utils.models import BaseDate
 
 
 class Glycemia(BaseDate):
-    person = models.ForeignKey("person.Person", on_delete=models.CASCADE)
+    person = models.ForeignKey(
+        "person.Person",
+        on_delete=models.CASCADE,
+        related_name="glycemias",
+    )
     value = models.PositiveSmallIntegerField()
-
-
